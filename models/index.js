a10c001a05a44567b5f37162ad19173c1cbd36c8
M8MBA <sauce.ix@gmail.com>
2022-05-17 16:05:50 -0600

    comments

--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
 const Pizza = require('./Pizza');
+// import the Comment.js model into models/index.js to package up both models
+const Comment = require('./Comment');
 
-module.exports = { Pizza };
+module.exports = { Pizza, Comment };
